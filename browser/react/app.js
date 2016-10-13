556b2bf5361c4b437124f761bbfa1f05f85d7741
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2016-10-13 12:42:54 -0400

    set up original routes in app.js

--- browser/react/app.js
+++ browser/react/app.js
@@ -5,10 +5,20 @@ import ReactDOM from 'react-dom';
 import store from './store';
 import { Provider } from 'react-redux';
 import AppContainer from './containers/AppContainer';
+import { Router, Route, hashHistory} from 'react-router';
+
+import AlbumsContainer from './containers/AlbumsContainer'
+import ArtistsContainer from './containers/ArtistsContainer'
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppContainer /> 
+    <Router history={hashHistory}>
+      <Route path="/" component={AppContainer}>
+        <Route path="/albums" component={AlbumsContainer}/>
+        <Route path="/artists" component={ArtistsContainer}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
