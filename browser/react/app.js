94d22cda440ce40c9c8ab56a9a48b0276e5b8962
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2016-10-13 13:12:27 -0400

    setting up hook within album.js

--- browser/react/app.js
+++ browser/react/app.js
@@ -4,19 +4,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
 import { Provider } from 'react-redux';
-import AppContainer from './containers/AppContainer';
-import { Router, Route, hashHistory} from 'react-router';
+import { Router, Route, hashHistory, IndexRoute} from 'react-router';
 
-import AlbumsContainer from './containers/AlbumsContainer'
-import ArtistsContainer from './containers/ArtistsContainer'
+import AppContainer from './containers/AppContainer';
+import AlbumsContainer from './containers/AlbumsContainer';
+import ArtistsContainer from './containers/ArtistsContainer';
+import AlbumContainer from './containers/AlbumContainer';
+import ArtistContainer from './containers/ArtistContainer';
+
 
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path="/" component={AppContainer}>
+        <IndexRoute component={AlbumsContainer}/>
         <Route path="/albums" component={AlbumsContainer}/>
         <Route path="/artists" component={ArtistsContainer}/>
+        <Route path="/albums/:albumId" component={AlbumContainer}/>
       </Route>
     </Router>
   </Provider>,
--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -3,12 +3,27 @@
 import React from 'react';
 import SongsContainer from '../containers/SongsContainer';
 
-export default ({ selectedAlbum }) => (
-  <div className="album">
-    <div>
-      <h3>{ selectedAlbum.name }</h3>
-      <img src={ selectedAlbum.imageUrl } className="img-thumbnail" />
-    </div>
-    <SongsContainer songs={selectedAlbum.songs} />
-  </div>
-);
+export default class Album extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    fetch(`/api/albums/${this.props.params.albumId}`)
+    .then(res=> res.json())
+    .then(album => console.log( "ALBUM", album ))
+  }
+
+  render() {
+    return (
+      <div className="album">
+      {    console.log(this.props.params.albumId)}
+        <div>
+          <h3>{ selectedAlbum.name }</h3>
+          <img src={ selectedAlbum.imageUrl } className="img-thumbnail" />
+        </div>
+        <SongsContainer songs={selectedAlbum.songs} />
+      </div>
+  )}
+};
